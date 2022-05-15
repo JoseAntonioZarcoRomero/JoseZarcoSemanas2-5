6ea26b6cbabf61ef19fe2968f28025fa874824a2
JoseAntonioZarcoRomero <zarco_romero_jose_antonio@comunidad.unam.mx>
2022-05-14 21:23:09 -0500

    Hora de volverse loleros

--- Semana3/LoL/dynamics/JS/index.js
+++ Semana3/LoL/dynamics/JS/index.js
@@ -18,9 +18,6 @@ class Campeon {
         console.log('Mi nombre es: '+this.nombre+', soy un campeón de League of Leyends');
         console.log('Mis grandiosas estadísticas son las siguientes; Vida:'+this.vida+', Maná:'+this.mana+', Ataque:'+this.ataque);
         console.log('Esta es la lista de mis habilidades: '+this.habilidades);
-        /*for(let valor of this.habilidades){
-            console.log(valor);
-        }*/
     }
     comparar(Campeon){
         if(Campeon.tipo == this.tipo){
@@ -38,8 +35,9 @@ class Campeon {
 /*Subclases
 --------------------------------------------------------------------*/
 class Tirador extends Campeon {
-    constructor(nombre,vida,ataque,mana,habilidades,tipo,fuerte1,fuerte2){
-        super(nombre,vida,ataque*=1.24,mana,habilidades='Distancia',tipo="Tirador",fuerte1="Tanque",fuerte2="Luchador");
+    constructor(nombre,vida,ataque,mana,distancia,habilidades,tipo,fuerte1,fuerte2){
+        super(nombre,vida,ataque*=1.24,mana,habilidades,tipo="Tirador",fuerte1="Tanque",fuerte2="Luchador");
+        this.distancia = distancia;
     }
 }
 
@@ -50,23 +48,30 @@ class Asesino extends Campeon {
 }
 
 class Mago extends Campeon {
-    constructor(nombre,vida,ataque,mana,habilidades,tipo,fuerte1,fuerte2){
-        super(nombre,vida,ataque,mana*=1.24, habilidades='Distancia',tipo="Mago",fuerte1="Tirador",fuerte2="Luchador");
+    constructor(nombre,vida,ataque,mana,distancia,habilidades,tipo,fuerte1,fuerte2){
+        super(nombre,vida,ataque,mana*=1.24, habilidades,tipo="Mago",fuerte1="Tirador",fuerte2="Luchador");
+        this.distancia = distancia;
     }
 }
 
 class Tanque extends Campeon {
-    constructor(nombre,vida,ataque,mana,habilidades,tipo,fuerte1,fuerte2){
-        super(nombre,vida*=1.24,ataque,mana,habilidades='Armadura',tipo="Tanque",fuerte1="Mago",fuerte2="Asesino");
+    constructor(nombre,vida,ataque,mana,armadura,habilidades,tipo,fuerte1,fuerte2){
+        super(nombre,vida*=1.24,ataque,mana,habilidades,tipo="Tanque",fuerte1="Mago",fuerte2="Asesino");
+        this.armadura = armadura;
     }
 }
 
 class Luchador extends Campeon {
-    constructor(nombre,vida,ataque,mana,habilidades,tipo,fuerte1,fuerte2){
-        super(nombre,vida*=1.15,ataque*=1.15,mana,habilidades='Movilidad',tipo="Luchador",fuerte1="Asesino",fuerte2="Tanque");
+    constructor(nombre,vida,ataque,mana,movilidad,habilidades,tipo,fuerte1,fuerte2){
+        super(nombre,vida*=1.15,ataque*=1.15,mana,habilidades,tipo="Luchador",fuerte1="Asesino",fuerte2="Tanque");
+        this.movilidad = movilidad;
     }
 }
 
 /*Instanciamiento de objetos
 --------------------------------------------------------------------*/
-//const pato = new Mago('pato',100,100,100,100, 'no sabe js', 'le gusta git');
+const Tristana = new Tirador('Tristana',100,100,100,200,'Disparar');
+const Pyke = new Asesino('Pyke',100,130,100,'Cazar');
+const Jayce = new Mago('Jayce',100,120,100,'Lanzar hechizos');
+const Sion = new Tanque('Sion',200,150,100,150,'Curación');
+const Skarner = new Luchador('Skarner',150,145,120,130,'Pelear');
